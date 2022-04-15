a4e1589d466d3f9091bf818b7766632b0d76c540
John-Thailand <hamoban69@gmail.com>
2022-04-15 09:47:22 +0900

    get.putをrunApp前に実行

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 // MaterialApp を GetMaterialAppに置き換える
-void main() => runApp(GetMaterialApp(home: Home()));
+void main() {
+  // Get.put() を使ってクラスをインスタンス化し、配下のウィジェットで利用できるようにする。
+  // ほとんど全ページで使うコントローラーである場合、main()でrunApp()する前にGet.putする。
+  final Controller c = Get.put(Controller());
+  runApp(GetMaterialApp(home: Home()));
+}
 
 class Controller extends GetxController {
-  // obs をつけると変更を監視できるようになる。
+  // obs（observer：観察者） をつけると変更を監視できるようになる。
   var count = 0.obs;
   increment() => count++;
 }
@@ -13,8 +18,8 @@ class Controller extends GetxController {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Get.put() を使ってクラスをインスタンス化し、配下のウィジェットで利用できるようにする。
-    final Controller c = Get.put(Controller());
+    // 他のページで使われているコントローラーを見つけて、それを使用できる。
+    final Controller c = Get.find();
 
     return Scaffold(
       // カウントが変更されるたびに Text()を更新する
